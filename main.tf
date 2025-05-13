ab48297641abfd8e9e6f2b4214327aa0304a959f
Himanshu <137988128+Himansri21@users.noreply.github.com>
2025-05-13 14:03:08 +0530

    Update main.tf to use desired template

--- main.tf
+++ main.tf
@@ -1,15 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_launch_template" "example" {
-  name_prefix   = "example-launch-template"
-  image_id      = "ami-0c55b159cbfafe1f0" # Replace with a valid AMI
-  instance_type = "t2.micro"
-
-  lifecycle {
-    create_before_destroy = true
-  }
+  region = "eu-central-1"
 }
 
 resource "aws_autoscaling_group" "example" {
@@ -19,13 +9,8 @@ resource "aws_autoscaling_group" "example" {
   vpc_zone_identifier  = ["subnet-0123456789abcdef0"] # Replace with your subnet
 
   launch_template {
-    id      = aws_launch_template.example.id
+    id      = "lt-09c4bf34fffc56cd8"
     version = "$Latest"
   }
-
-  tag {
-    key                 = "Name"
-    value               = "example-asg"
-    propagate_at_launch = true
-  }
 }
+
