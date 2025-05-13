b0223a0cf526f32caa9e390e38f61b6c8450e02d
Himanshu <137988128+Himansri21@users.noreply.github.com>
2025-05-13 14:07:49 +0530

    Update main.tf again

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_autoscaling_group" "example" {
+resource "aws_autoscaling_group" "Task-ASG-graphana-terraform" {
   desired_capacity     = 2
   max_size             = 3
   min_size             = 1
-  vpc_zone_identifier  = ["subnet-0123456789abcdef0"] # Replace with your subnet
+  vpc_zone_identifier  = ["subnet-040215eb6e71489b6"] 
 
   launch_template {
     id      = "lt-09c4bf34fffc56cd8"
